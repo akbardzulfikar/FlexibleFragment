40ee2bea44f2dff8173e17201146b6761f54e3e7
laksamana.akbar <laksamana.guntur@mekar.id>
2020-09-14 21:53:46 +0700

    Second Commit,
    Add new Fragment, using fragmentManager to get FragmentManager to start Fragment Transaction, by using replace() will change the fragment from HomeFragment.kt to CategoryFragment.kt, addToBackStack(null) on HomeFragment.kt to return from CategoryFragment.kt to HomeFragment.kt using back Button

--- app/src/main/java/co/id/flexiblefragment/HomeFragment.kt
+++ app/src/main/java/co/id/flexiblefragment/HomeFragment.kt
@@ -10,6 +10,15 @@ import android.widget.Button
 
 class HomeFragment : Fragment(), View.OnClickListener {
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btnCategory:Button = view.findViewById(R.id.btn_category)
@@ -17,12 +26,18 @@ class HomeFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
+        if (v.id == R.id.btn_category) {
+            val mCategoryFragment = CategoryFragment()
+            val mFragmentManager = fragmentManager
+            mFragmentManager?.beginTransaction()?.apply {
+                replace(
+                    R.id.frame_container,
+                    mCategoryFragment,
+                    CategoryFragment::class.java.simpleName
+                )
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)    }
 }
